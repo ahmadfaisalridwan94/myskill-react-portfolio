92994edb75dae1d8e642965499bb2cde3494a5c9
ASUS <ahmadfaisal.ridwan@gmail.com>
2024-05-10 11:01:53 +0700

    add Resume section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <section id="footer">Footer</section>
+            <div>Footer</div>
         )
     }
 }
